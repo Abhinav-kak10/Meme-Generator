ef8ce4eb50dc54f24dab60a7261f1684515bc327
Abhinav-kak10 <88281326+Abhinav-kak10@users.noreply.github.com>
2023-09-11 01:59:40 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import React from "react"
+import Header from "./component/Header"
+import Meme from "./component/Meme"
+import './style.css';
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <Meme />
+        </div>
+    )
+}
